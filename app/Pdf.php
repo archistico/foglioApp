52a6df6a73ddc87dc5a3861a9d27594207177f6f
unknown <info@archistico.com>
2019-09-15 15:50:57 +0200

    ok print value table

--- app/Pdf.php
+++ app/Pdf.php
@@ -17,60 +17,59 @@ class Pdf
     public function ViewPdf()
     {
         /*
-        $pdf = new FPDF();
+    $pdf = new FPDF();
 
-        $pageWidth = 297;
-        $pageHeight = 210;
-        $margin = 7;
-        $gutter = 2;
-        $header = 10;
+    $pageWidth = 297;
+    $pageHeight = 210;
+    $margin = 7;
+    $gutter = 2;
+    $header = 10;
 
-        $numberDayInWeek = count($this->app);
-        $numberDays = 2;
-        $width = $pageWidth - 2 * $margin;
-        $height = $pageHeight - 2 * $margin;
-        $numberColumns = $numberDayInWeek*$numberDays;
-        $columnWidth = ($width - ($numberColumns-1) * $gutter) / $numberColumns;
-        $columnHeight = $height - $header;
-        $rowMargin = 1;
-        $numberRows = 31;
-        $rowHeight = ($columnHeight - 1 * $rowMargin) / $numberRows;
-        $rowPadding = 0.5;
-        $rowWidthNumber = 5;
-        $textNumberHeight = 3;
-        $textTitleHeight = 16;
-        $textTitleColumnHeight = 11;
-        $textRowHeight = 10;
+    $numberDayInWeek = count($this->app);
+    $numberDays = 2;
+    $width = $pageWidth - 2 * $margin;
+    $height = $pageHeight - 2 * $margin;
+    $numberColumns = $numberDayInWeek*$numberDays;
+    $columnWidth = ($width - ($numberColumns-1) * $gutter) / $numberColumns;
+    $columnHeight = $height - $header;
+    $rowMargin = 1;
+    $numberRows = 31;
+    $rowHeight = ($columnHeight - 1 * $rowMargin) / $numberRows;
+    $rowPadding = 0.5;
+    $rowWidthNumber = 5;
+    $textNumberHeight = 3;
+    $textTitleHeight = 16;
+    $textTitleColumnHeight = 11;
+    $textRowHeight = 10;
 
-        $pdf->AddPage('L', [$pageWidth, $pageHeight]);
-        $pdf->SetMargins($margin, $margin, $margin);
+    $pdf->AddPage('L', [$pageWidth, $pageHeight]);
+    $pdf->SetMargins($margin, $margin, $margin);
 
-        $pdf->SetFont('Arial', 'B', $textTitleHeight);
-        $text = "Appuntamenti Rollandin";
-        $textWidth = strlen($text) * 1;
-        $pdf->Text($margin + $width / 2 - $textWidth, $margin + $header / 2, $text);
+    $pdf->SetFont('Arial', 'B', $textTitleHeight);
+    $text = "Appuntamenti Rollandin";
+    $textWidth = strlen($text) * 1;
+    $pdf->Text($margin + $width / 2 - $textWidth, $margin + $header / 2, $text);
 
-        $giorni = array();
-        for($c = 0; $c < $numberDays; $c++) {
-            foreach(array_keys($this->app) as $g) {
-                $giorni[] = $g;
-            }
-        }
+    $giorni = array();
+    for($c = 0; $c < $numberDays; $c++) {
+    foreach(array_keys($this->app) as $g) {
+    $giorni[] = $g;
+    }
+    }
 
-        for ($c = 0; $c < $numberColumns; $c++) {
-            $xColumn = $margin + $c * ($columnWidth + $gutter);
-            $yColumn = $margin + $header;
-            $pdf->SetFillColor(196);
-            $pdf->Rect($xColumn,$yColumn,$columnWidth,$columnHeight,'F');
-            
-            $pdf->SetFont('Arial', 'B', $textTitleColumnHeight);
-            $pdf->Text($xColumn, $yColumn, $giorni[$c]);
+    for ($c = 0; $c < $numberColumns; $c++) {
+    $xColumn = $margin + $c * ($columnWidth + $gutter);
+    $yColumn = $margin + $header;
+    $pdf->SetFillColor(196);
+    $pdf->Rect($xColumn,$yColumn,$columnWidth,$columnHeight,'F');
 
-            
-        }
+    $pdf->SetFont('Arial', 'B', $textTitleColumnHeight);
+    $pdf->Text($xColumn, $yColumn, $giorni[$c]);
 
-        $pdf->Output();
-        */
+    }
+
+    $pdf->Output();
+     */
     }
 
     public function Screen()
@@ -85,8 +84,8 @@ class Pdf
         $numberDays = $this->numberDays;
         $width = $pageWidth - 2 * $margin;
         $height = $pageHeight - 2 * $margin;
-        $numberColumns = $numberDayInWeek*$numberDays;
-        $columnWidth = ($width - ($numberColumns-1) * $gutter) / $numberColumns;
+        $numberColumns = $numberDayInWeek * $numberDays;
+        $columnWidth = ($width - ($numberColumns - 1) * $gutter) / $numberColumns;
         $columnHeight = $height - $header;
         $rowMargin = 1;
         $numberRows = 31;
@@ -100,29 +99,69 @@ class Pdf
 
         $text = "Appuntamenti Rollandin";
         $textWidth = strlen($text) * 1;
-        
-        $set = new CalcoloSettimana($this->data);
-        $corrispondenza = [["Lu", $set->lunedi], ["Ma", $set->martedi],  ["Me", $set->mercoledi],  ["Gi", $set->giovedi],  ["Ve", $set->venerdi],  ["Sa", $set->sabato],  ["Do", $set->domenica]];
 
-        echo $text.BR;
+        $t = $this->data;
+        $corrispondenza = array();
+
+        for ($c = 0; $c < $numberDays; $c++) {
+            if ($c != 0) {
+                $t->add(new \DateInterval('P1D'));
+            }
+            $set = new CalcoloSettimana($t);
+            $corrispondenza[] = ["Lu", $set->lunedi];
+            $corrispondenza[] = ["Ma", $set->martedi];
+            $corrispondenza[] = ["Me", $set->mercoledi];
+            $corrispondenza[] = ["Gi", $set->giovedi];
+            $corrispondenza[] = ["Ve", $set->venerdi];
+            $corrispondenza[] = ["Sa", $set->sabato];
+            $corrispondenza[] = ["Do", $set->domenica];
+        }
+
+        echo $text . BR;
 
         $giorni = array();
-        for($c = 0; $c < $numberDays; $c++) {
-            foreach(array_keys($this->app) as $g) {
+        for ($c = 0; $c < $numberDays; $c++) {
+            foreach (array_keys($this->app) as $g) {
                 $giorni[] = $g;
             }
         }
 
+        //echo PRE1.var_dump($this->app).PRE2;
+        // echo PRE1 . print_r($this->app) . PRE2;
+
         for ($c = 0; $c < $numberColumns; $c++) {
-            
+
             $data_stringa = "";
-            foreach($corrispondenza as $corr) {
-                if($giorni[$c] == $corr[0]) {
-                    $data_stringa = $corr[1]->format('d-m-Y');
+
+            for ($i = 0; $i < count($corrispondenza); $i++) {
+                if ($giorni[$c] == $corrispondenza[$i][0]) {
+                    $data_stringa = $corrispondenza[$i][1]->format('d-m-Y');
+                    $corrispondenza[$i][0] = null;
+                    $corrispondenza[$i][1] = null;
+                    break;
                 }
-            }           
-            
-            echo $giorni[$c]." - ".$data_stringa.BR;            
+            }
+
+            echo $giorni[$c] . " - " . $data_stringa . BR;
+
+            foreach ($this->app as $k => $v) {
+                if($k == $giorni[$c]) {
+                    $t_amb_prec = "";
+                    foreach($v as $ora) {
+                        $t_ora = $ora->getOra();
+                        $t_amb = $ora->getAmbulatorio();
+                        if($t_amb_prec != $t_amb) {
+                            $t_amb_prec = $t_amb;
+                            echo $t_amb.BR;    
+                        }
+                        echo $t_ora.BR;
+                    }
+                }
+
+            }
+
+            //echo PRE1.print_r($this->app).PRE2;
+            echo BR; 
         }
     }
 }
--- index.php
+++ index.php
@@ -4,8 +4,11 @@ DEFINE('BR', "<br>");
 DEFINE('PRE1', "<pre>");
 DEFINE('PRE2', "</pre>");
 
-$data = DateTime::createFromFormat('Y-m-d', $_GET['data']);
-
+if(isset($_GET['data'])) {
+    $data = DateTime::createFromFormat('Y-m-d', $_GET['data']);
+} else {
+    $data = new DateTime;
+}
 
 $suddivisione_oraria = ["00", "15", "30", "45"];
 $orari = new App\Orari();
@@ -28,45 +31,3 @@ $pdf = new App\Pdf($app->appuntamenti, 2, $data);
 $pdf->Screen();
 //$pdf->ViewPdf();
 
-/*
-
-        $settimana = new \App\Settimana($params['data']);
-        $listaGiorni = new \App\ListaGiorni();
-        //$listaGiorni->Add(new \App\Giorno($settimana->lunedi->format('d/m/Y'), 'Lunedì'));
-        $listaGiorni->Add(new \App\Giorno($settimana->martedi->format('d/m/Y'), 'Martedì'));
-        $listaGiorni->Add(new \App\Giorno($settimana->mercoledi->format('d/m/Y'), 'Mercoledì'));
-        $listaGiorni->Add(new \App\Giorno($settimana->giovedi->format('d/m/Y'), 'Giovedì'));
-        //$listaGiorni->Add(new \App\Giorno($settimana->venerdi->format('d/m/Y'), 'Venerdì'));
-        $listaOrari = new \App\ListaOrari();
-        $listaOrari->Add(new \App\Orario('Lunedì', '8:00', '', false));
-        $listaOrari->Add(new \App\Orario('Lunedì', '8:15', '', false));
-        $listaOrari->Add(new \App\Orario('Lunedì', '8:30', '', false));
-        $listaOrari->Add(new \App\Orario('Lunedì', '8:45', '', false));
-        $listaOrari->Add(new \App\Orario('Lunedì', '9:00', '', false));
-        $listaOrari->Add(new \App\Orario('Lunedì', '9:15', '', false));
-        $listaOrari->Add(new \App\Orario('Lunedì', '9:30', '', false));
-        $listaOrari->Add(new \App\Orario('Mercoledì', '11:30', 'Chatillon', true));
-        $listaOrari->Add(new \App\Orario('Mercoledì', '11:45', 'Chatillon', true));
-        $listaOrari->Add(new \App\Orario('Mercoledì', '12:00', 'Chatillon', true));
-        
-        // Data Orario Persona Nota Fatto Assente Annullato
-        $listaAppuntamenti = new \App\ListaAppuntamenti();
-        $db = new \DB\SQL('sqlite:db/database.sqlite');
-        $sql = "SELECT * FROM appuntamenti WHERE annullato = 0 AND fatto = 0 AND assente = 0";
-        $appuntamentiDB = $db->exec($sql);
-        foreach ($appuntamentiDB as $appuntamentoDB) {
-            $str = jdtojulian($appuntamentoDB['data']);
-            $dmy = \DateTime::createFromFormat('m/d/Y', $str)->format('d/m/Y');
-            $listaAppuntamenti->Add(new \App\Appuntamento($dmy, $appuntamentoDB['ora'], $appuntamentoDB['persona'], $appuntamentoDB['note'], $appuntamentoDB['annullato'], $appuntamentoDB['assente'], $appuntamentoDB['fatto'], $appuntamentoDB['inizio']));
-        }
-        $tabella = new Tabella($listaGiorni, $listaOrari, $listaAppuntamenti);
-        $f3->set('tabella', $tabella->ToArray());
-        $f3->set('lunedi', $settimana->lunedi->format('d-m-Y'));
-        $f3->set('domenica', $settimana->domenica->format('d-m-Y'));
-        $f3->set('lunediPrecedente', $settimana->lunediPrecedente->format('d-m-Y'));
-        $f3->set('lunediSuccessivo', $settimana->lunediSuccessivo->format('d-m-Y'));
-        $f3->set('titolo', 'Appuntamenti');
-        $f3->set('script', 'appuntamenti.js');
-        $f3->set('contenuto', 'appuntamenti.htm');
-        echo \Template::instance()->render('templates/base.htm');
-*/
