a255694398ef4857459bd70ea1249a37abba4011
archistico <info@archistico.com>
2019-11-01 18:07:42 +0100

    add ore vuote cav

--- foglioapp.php
+++ foglioapp.php
@@ -36,13 +36,13 @@ switch ($dott) {
         $orari->OrarioByArray(8, 19, ["00", "15", "30", "40", "50"]);
         
         $app = new App\Settimana($orari);
-        $app->AddGiornata("Lu", "St-Vincent", "15:30", "19:00", [])
-            ->AddGiornata("Ma", "St-Vincent", "15:30", "19:00", [])
-            ->AddGiornata("Me", "St-Vincent", "9:30", "12:30", [])
-            ->AddGiornata("Gi", "St-Vincent", "10:00", "12:00", [])
-            ->AddGiornata("Ve", "St-Vincent", "9:30", "12:30", []);
+        $app->AddGiornata("Lu", "St-Vincent", "15:30", "19:00", ["17:15" => "---", "18:50" => "---"])
+            ->AddGiornata("Ma", "St-Vincent", "15:30", "19:00", ["17:15" => "---", "18:50" => "---"])
+            ->AddGiornata("Me", "St-Vincent", "9:30", "12:30", ["10:30" => "---", "12:15" => "---"])
+            ->AddGiornata("Gi", "St-Vincent", "10:00", "12:00", ["11:00" => "---", "11:50" => "---"])
+            ->AddGiornata("Ve", "St-Vincent", "9:30", "12:30", ["10:30" => "---", "12:15" => "---"]);
 
-        $pdf = new App\Pdf($app->appuntamenti, 1, $data, "Dott.ssa Cavurina Rosanna - 0166.51.23.28 - 338.89.20.536");
+        $pdf = new App\Pdf($app->appuntamenti, 1, $data, "Dott.ssa Cavurina Rosanna - 0166.51.23.28 - 338.89.20.536 - 348.16.86.019");
         break;
 }
 
